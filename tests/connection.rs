09656350cd2638cd6d75f61697a6267033d0f321
Hayden Faulds <fauldsh@gmail.com>
2016-04-13 19:57:47 +0100

    tidy up

--- tests/connection.rs
+++ tests/connection.rs
@@ -16,58 +16,59 @@ mod tests {
             let mut writer = Cursor::new(Vec::new());
         }
 
-        it "listen_stops_for_quit_command" {
-            let mut reader = Cursor::new(b"QUIT");
-
-            let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
-
-            connection.listen();
+        describe! listen {
+            it "listen_stops_for_quit_command" {
+                let mut reader = Cursor::new(b"QUIT");
+                let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
+                connection.listen();
+            }
         }
 
-        it "process_message_executes_push" {
-            let mut reader = Cursor::new(b"PUSH 'queue' 'data';");
+        describe! process_message {
+            it "process_message_executes_push" {
+                let mut reader = Cursor::new(b"PUSH 'queue' 'data';");
 
-            {
-                let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
-                connection.process_message();
+                {
+                    let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
+                    connection.process_message();
+                }
+
+                assert_eq!(_queue.pop_front(), Some("data".to_string()));
             }
 
-            assert_eq!(_queue.pop_front(), Some("data".to_string()));
-        }
+            it "process_message_outputs_success_for_push" {
+                let mut reader = Cursor::new(b"PUSH 'queue' 'data';");
 
-        it "process_message_outputs_success_for_push" {
-            let mut reader = Cursor::new(b"PUSH 'queue' 'data';");
+                {
+                    let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
+                    connection.process_message();
+                }
 
-            {
-                let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
-                connection.process_message();
-            }
-            assert_eq!(String::from_utf8(writer.into_inner()).unwrap(), "SUCCESS\r\n".to_string());
-        }
-
-        it "process_message_executes_pop" {
-            let mut reader = Cursor::new(b"POP 'queue';");
-
-            _queue.push_back("data".to_string());
-
-            {
-                let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
-                connection.process_message();
+                assert_eq!(String::from_utf8(writer.into_inner()).unwrap(), "SUCCESS\r\n".to_string());
             }
 
-            assert_eq!(_queue.pop_front(), None);
-        }
+            it "process_message_executes_pop" {
+                let mut reader = Cursor::new(b"POP 'queue';");
+                _queue.push_back("data".to_string());
 
-        it "process_message_outputs_value_for_pop" {
-            let mut reader = Cursor::new(b"POP 'queue';");
+                {
+                    let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
+                    connection.process_message();
+                }
 
-            _queue.push_back("data".to_string());
-
-            {
-                let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
-                connection.process_message();
+                assert_eq!(_queue.pop_front(), None);
             }
-            assert_eq!(String::from_utf8(writer.into_inner()).unwrap(), "data\r\n".to_string());
+
+            it "process_message_outputs_value_for_pop" {
+                let mut reader = Cursor::new(b"POP 'queue';");
+                _queue.push_back("data".to_string());
+
+                {
+                    let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
+                    connection.process_message();
+                }
+                assert_eq!(String::from_utf8(writer.into_inner()).unwrap(), "data\r\n".to_string());
+            }
         }
     }
 }
