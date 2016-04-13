2e95cee15c0354a174384472501064b0f9516c13
Hayden Faulds <fauldsh@gmail.com>
2016-04-13 16:15:19 +0100

    make use of before block

--- tests/connection.rs
+++ tests/connection.rs
@@ -7,11 +7,16 @@ mod tests {
     pub use self::queue_experiments::connection::{Connection};
     pub use self::queue_experiments::queue_table::{QueueTable};
     pub use std::io::Cursor;
+
     describe! connection {
+        before_each {
+            let queue_table = QueueTable::new();
+            let _queue = queue_table.get_or_create_queue("queue".to_string());
+
+            let mut writer = Cursor::new(Vec::new());
+        }
 
         it "listen_stops_for_quit_command" {
-            let queue_table = QueueTable::new();
-            let mut writer = Cursor::new(Vec::new());
             let mut reader = Cursor::new(b"QUIT");
 
             let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
@@ -20,8 +25,6 @@ mod tests {
         }
 
         it "process_message_executes_push" {
-            let queue_table = QueueTable::new();
-            let mut writer = Cursor::new(Vec::new());
             let mut reader = Cursor::new(b"PUSH 'queue' 'data';");
 
             {
@@ -29,13 +32,10 @@ mod tests {
                 connection.process_message();
             }
 
-            let queue = queue_table.get_queue(&"queue".to_string()).unwrap();
-            assert_eq!(queue.pop_front(), Some("data".to_string()));
+            assert_eq!(_queue.pop_front(), Some("data".to_string()));
         }
 
         it "process_message_outputs_success_for_push" {
-            let queue_table = QueueTable::new();
-            let mut writer = Cursor::new(Vec::new());
             let mut reader = Cursor::new(b"PUSH 'queue' 'data';");
 
             {
@@ -46,29 +46,22 @@ mod tests {
         }
 
         it "process_message_executes_pop" {
-            let queue_table = QueueTable::new();
-            let mut writer = Cursor::new(Vec::new());
             let mut reader = Cursor::new(b"POP 'queue';");
 
-            let queue = queue_table.get_or_create_queue("queue".to_string());
-            queue.push_back("data".to_string());
+            _queue.push_back("data".to_string());
 
             {
                 let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
                 connection.process_message();
             }
 
-            let queue = queue_table.get_queue(&"queue".to_string()).unwrap();
-            assert_eq!(queue.pop_front(), None);
+            assert_eq!(_queue.pop_front(), None);
         }
 
         it "process_message_outputs_value_for_pop" {
-            let queue_table = QueueTable::new();
-            let mut writer = Cursor::new(Vec::new());
             let mut reader = Cursor::new(b"POP 'queue';");
 
-            let queue = queue_table.get_or_create_queue("queue".to_string());
-            queue.push_back("data".to_string());
+            _queue.push_back("data".to_string());
 
             {
                 let mut connection = Connection::new(&mut reader, &mut writer, &queue_table);
